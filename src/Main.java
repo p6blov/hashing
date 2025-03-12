54684de8632434ba9383ba715f467e4022647eb9
p6blov <pablo.vasquez5002@outlook.com>
2025-03-12 17:54:59 -0400

    Class revision; main and logic separation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hashTable table = new hashTable();
+
+        int[] nums = {10, 20, 15, 5, 25, 30};
+        fillLinear(table, nums);
+        table.print("Linear");
+
+        hashTable table1 = new hashTable();
+        int[] num1 = {99, 77, 55, 66, 88, 44, 22};
+        fillQuadratic(table1, num1);
+        table.print("Quadratic");
+
+        hashTable table2 = new hashTable();
+        int[] num2 = {1, 2, 3, 4, 5, 6, 7};
+        fillDouble(table2, num2);
+        table.print("Double Hash");
+    }
+    public static void fill(hashTable table, int[] nums, String method) {
+        for(int n : nums)
+            System.out.println("Able to add: " + table.insert(n, method));
+    }
+    public static void fillLinear(hashTable table, int[] nums) {
+        for(int n : nums)
+            table.insert(n, "Linear");
+    }
+    public static void fillQuadratic(hashTable table, int[] nums) {
+        for(int n : nums)
+            table.insert(n, "Quadratic");
+    }
+    public static void fillDouble(hashTable table, int[] nums) {
+        for(int n : nums)
+            table.insert(n, "Double Hash");
+    }
+    public static void fillAll(hashTable table, int[] nums) {
+        fillLinear(table, nums);
+        table.print("Linear");
+        table.empty();
+
+        fillQuadratic(table, nums);
+        table.print("Quadratic");
+        table.empty();
+
+        fillDouble(table, nums);
+        table.print("Double Hash");
+        table.empty();
     }
 }
---
+++ src/hashTable.java
@@ -0,0 +1,100 @@
+import java.util.Arrays;
+
+class hashTable {
+    private int size;
+    private int[] table;
+
+    public hashTable(int size) {
+        this.size = size;
+        table = new int[size];
+        Arrays.fill(table, -1);
+    }
+    public hashTable() {
+        table = new int[10];
+        Arrays.fill(table, -1);
+        this.size = table.length;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void empty() {
+        Arrays.fill(table, -1);
+    }
+
+    private int hash1(int key) {
+        return key % size;
+    }
+
+    private int hash2(int key) {
+        return key % (size - 1);
+    }
+
+    private int getLinearProbIndex(int key, int i) {
+        return (hash1(key) + i * i) % size;
+    }
+
+    private int getQuadraticProbIndex(int key, int i) {
+        return (hash1(key) + i * i) % size;
+    }
+
+    private int getDoubleHash(int key, int i) {
+        return (hash1(key) + i * hash2(key) % size);
+    }
+
+    private String method(String method) {
+        return method.toLowerCase().substring(0, 1);
+    }
+
+    private int getIndex(int key, String m) {
+        for (int i = 0; i < size; i++) {
+            int index = switch (method(m)) {
+                case "l" -> getLinearProbIndex(key, i);
+                case "q" -> getQuadraticProbIndex(key, i);
+                case "d" -> getDoubleHash(key, i);
+                default -> throw new IllegalArgumentException("Invalid probing method. Please try again.");
+            };
+            if (table[index] == -1) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public boolean insert(int key, String method) {
+        int index = getIndex(key, method);
+        if (table[index] == -1) {
+            table[index] = key;
+            return true;
+        }
+        return false;
+    }
+    public boolean delete(int key, String method) {
+        int index = getIndex(key, method);
+        if(table[index] == key) {
+            table[index] = -1;
+            return true;
+        }
+        return false;
+    }
+    public boolean search(int key, String method) {
+        int index = getIndex(key, method);
+        return table[index] == key;
+    }
+    public void print() {print("");}
+    public void print(String method) {
+        String out = method.isEmpty() ? "Hash Table" : "Hash Table: " + method;
+        for(int i = 0; i < size; i++) {
+            out+= "\n" + i + ": ";
+            if(table[i] != -1){
+                out+= table[i];
+            }
+        }
+        System.out.println(out);
+    }
+}

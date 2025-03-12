d7de3217eb2d77c6426a92facc9ba226f127399e
p6blov <pablo.vasquez5002@outlook.com>
2025-03-12 18:44:26 -0400

    Fixed output & cleaning up main method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         hashTable table = new hashTable();
-
         int[] nums = {10, 20, 15, 5, 25, 30};
         fillLinear(table, nums);
         table.print("Linear");
